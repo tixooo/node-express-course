3292769306046c54e018fd6e7ae0ba45e6f4961e
Konstantin Stefanov <konstantin.stefanov87@gmail.com>
2021-09-29 22:45:50 +0300

    Update app.js

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,15 +1,25 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const {products} = require('./data')
 
-app.use(express.static('./public'));
+app.get('/', (req, res)=>{
+    res.send('<h1> Home page</h1><a href="/api/products">products</a>');
+})
+app.get('/api/products', (req,res)=> {
+    const newProducts = products.map((product)=>{
+        const{id, name,image} = product;
+        return{id, name,image}
+    })
+    res.json(newProducts)
+})
+app.get('/api/products/:productID', (req,res)=> {
+    const {productID} = req.params;
 
-// app.get('/',(req,res)=>{
-//     res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
-// })
-
-app.all('*', (req,res)=>{
-   res.status(404).send('resource not found'); 
+    const singleProduct = products.find(product=> product.id === Number(productID))
+    if(!singleProduct) {
+        return res.status(404).send('Product does not exist');
+    }
+    res.json(singleProduct)
 })
 
 app.listen(5000, ()=>{
