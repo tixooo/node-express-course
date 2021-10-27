661fe6503c79103e9016c8283b86bdb4bb983e02
Konstantin Stefanov <konstantin.stefanov87@gmail.com>
2021-10-27 19:43:13 +0300

    finished the JWT Basics project
    getting 500 instead of bad request when entering no email/password, cant find the fix, need to look it up further

--- 05-JWT-Basics/final/middleware/auth.js
+++ 05-JWT-Basics/final/middleware/auth.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken')
-const { UnauthenticatedError } = require('../errors')
+const {
+  UnauthenticatedError
+} = require('../errors')
 
 const authenticationMiddleware = async (req, res, next) => {
   const authHeader = req.headers.authorization
@@ -9,11 +11,16 @@ const authenticationMiddleware = async (req, res, next) => {
   }
 
   const token = authHeader.split(' ')[1]
-
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    const { id, username } = decoded
-    req.user = { id, username }
+    const {
+      id,
+      username
+    } = decoded
+    req.user = {
+      id,
+      username
+    }
     next()
   } catch (error) {
     throw new UnauthenticatedError('Not authorized to access this route')
